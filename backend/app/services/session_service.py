9dbf829d859e8a7564d0457194ca026df826df46
Taegan Irving <156956832+Taeganirving@users.noreply.github.com>
2026-09-22 23:27:40 -0600

    session service change

--- backend/app/services/session_service.py
+++ backend/app/services/session_service.py
@@ -4,8 +4,15 @@ from uuid import uuid4
 from app.models.session import Session
 
 
+sessions: dict[str, Session] = {}
+
+
 def create_session() -> Session:
-    return Session(
+    session = Session(
         id=str(uuid4()),
         started_at=datetime.now(timezone.utc),
     )
+
+    sessions[session.id] = session
+
+    return session
