a2d5745f6fbd80120f3505980f100a7f9a5c9a3f
Taegan Irving <156956832+Taeganirving@users.noreply.github.com>
2026-09-22 23:31:35 -0600

    stop session

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,3 +19,15 @@ def health_check():
 @app.post("/sessions", response_model=Session)
 def start_session():
     return create_session()
+
+@app.post("/sessions/{session_id}/stop", response_model=Session)
+def end_session(session_id: str):
+    session = stop_session(session_id)
+
+    if session is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Session not found"
+        )
+
+    return session
